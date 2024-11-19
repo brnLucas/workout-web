5577d524412bfd463e80457688c6eb8f411af732
Breno Nascimento <breno.nascimento@fpf.br>
2024-11-19 01:07:28 -0400

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
-import path from "path"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    outDir: 'build', // ou o diretório que você preferir
+    emptyOutDir: true,
+    sourcemap: true
+  }
 })
